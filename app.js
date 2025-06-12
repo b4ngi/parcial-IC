4805dad7d77d830ec6c6b2e2bde4e7265da4e395
b4ngi <ignaciobenetucci@gmail.com>
2025-06-12 15:35:18 -0300

    prueba test exitoso

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Aplicación funcionando correctamente.');
+  res.send('Aplicación funcionando correctamente');
 });
 
 module.exports = app;
